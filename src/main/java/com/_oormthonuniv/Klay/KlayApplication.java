3bfb029fcd235fd3f77a5e41314a182ff24d3ed8
Seung Won Lee <devsolfe@gmail.com>
2024-11-18 01:14:41 +0900

    init: initalize spring project

---
+++ src/main/java/com/_oormthonuniv/Klay/KlayApplication.java
@@ -0,0 +1,13 @@
+package com._oormthonuniv.Klay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_oormthonuniv/Klay/KlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com._oormthonuniv.Klay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
