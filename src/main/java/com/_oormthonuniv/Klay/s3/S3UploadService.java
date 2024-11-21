ef7ad14d5bae3d0a8c0bd7e475ec7e3f3aeac7cb
Seung Won Lee <devsolfe@gmail.com>
2024-11-22 01:42:35 +0900

    feat: add S3 upload service

---
+++ src/main/java/com/_oormthonuniv/Klay/s3/S3UploadService.java
@@ -0,0 +1,36 @@
+package com._oormthonuniv.Klay.s3;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class S3UploadService {
+    private final AmazonS3Client amazonS3Client;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    @Value("${Klay.s3.folder}")
+    private String folder;
+
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = file.getOriginalFilename();
+            String fileUrl = "https://" + bucket + "/" + folder + "/" + fileName;
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+            return fileUrl;
+        } catch (Exception e) {
+            // todo: to fomal exception handling
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+}
