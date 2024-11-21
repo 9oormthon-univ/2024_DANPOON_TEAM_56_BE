410df1f0ec18295addf12245f39044feee68773d
Seung Won Lee <devsolfe@gmail.com>
2024-11-22 02:14:47 +0900

    fix: add Application dependency

--- src/test/java/com/_oormthonuniv/Klay/KlayApplicationTests.java
+++ src/test/java/com/_oormthonuniv/Klay/KlayApplicationTests.java
@@ -2,12 +2,14 @@ package com._oormthonuniv.Klay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class KlayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
